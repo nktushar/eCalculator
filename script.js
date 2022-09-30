f1bbeafbc994fc6a071ef2dc1bbd5f991c86c6a8
Nazmul Kabir <nazmulkabirtushar@gmail.com>
2022-10-01 01:59:01 +0600

    all done

---
+++ script.js
@@ -0,0 +1,30 @@
+const display = document.getElementById("display");
+
+const buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case 'CE':
+                if(display.innerText){
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                    break;
+                }
+                catch{
+                    display.innerText = "Error!!";
+                    break;
+                }
+            default:
+                display.innerText += e.target.innerText;
+        }
+    })
+})
